637980ed3d4511cc4faa2bba5be06b1b126f02f5
Michael <michaael.monir119@gmail.com>
2024-02-11 23:24:13 +0200

    adjusted the height of ticket picture

--- src/NewEvent/EventMiddleComponent/TicketsPage/TicketHeader.jsx
+++ src/NewEvent/EventMiddleComponent/TicketsPage/TicketHeader.jsx
@@ -11,12 +11,16 @@ export default function TicketHeader(props) {
             <div className={"col-9 d-flex flex-column"}>
                 <h2 >Lorem ipsum dolor.</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aut culpa eaque earum est ex explicabo ipsa labore laudantium libero modi, nihil numquam obcaecati, saepe, suscipit totam velit. Facilis!</p>
+
                 <div className={"d-flex justify-content-between align-items-end ticket-footer pb-2"}>
-                    <div className={"d-flex justify-content-start container-fluid"}>
-                        <p className={"m-1"}>Price: 100 EGP</p>
-                        <p className={"m-1"}>Quantity: 100</p>
+                    <div className={"justify-content-start container-fluid"}>
+                        <p className={"m-0 p-0"}>Price: 100 EGP</p>
+                        <div className={"d-flex"}>
+                            <p className={"me-1"}>Quantity: 100</p>
+                            <p className={"mx-1"}>Quantity: 100</p>
+                        </div>
                     </div>
-                    <div className={"d-flex justify-content-end container-fluid"}>
+                    <div className={"d-flex justify-content-end container-fluid mb-2"}>
                         <button className={"btn btn-primary m-1"}>Buy</button>
                         <button className={"btn btn-primary m-1"}>Refund</button>
                     </div>
